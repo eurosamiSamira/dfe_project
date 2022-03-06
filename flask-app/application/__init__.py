6cadfbd22d5cdac6149599529bb715fc9178b97f
Samira <eurosami@hotmail.com>
2022-03-06 21:54:32 +0000

    BLG-15: add mysql functionality

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -1,10 +1,19 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+URI = { 
+    'MYSQL_URI': os.getenv('MYSQL_URI'),
+    'MYSQL_ROOT_PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD')
+}
+
+print('mysql+pymysql://root:{MYSQL_ROOT_PASSWORD}@{MYSQL_URI}:3306/flask-db'.format(**URI))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:{MYSQL_ROOT_PASSWORD}@{MYSQL_URI}:3306/flask-db'.format(**URI)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hreuifh7ueyhf'
 
 db = SQLAlchemy(app)
+
+from application import routes
